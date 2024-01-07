b6144470be5e76977b2eef3044e43ce6d20975f0
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-01-07 02:35:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -557,5 +557,5 @@ app.post("/send", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
